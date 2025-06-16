ffd0dc4da8315608d0870507477f56b5c99d7d00
Rahnas p <rahnasx2@gmail.com>
2025-06-16 20:32:43 +0530

    move callback uri to .env

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv').config()
 passport.use(new googleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'/auth/google/callback'
+    callbackURL:process.env.GOOGLE_CALLBACK_URL
 },
 
 async (accessToken,refreshToken,profile,done)=>{
